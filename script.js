a8a6b7393d4a77c919a551fc9494cd4dd4bc259e
youssef1129 <youssefmaazouz79@gmail.com>
2021-09-02 16:29:41 +0100

    v-G

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(()=>{
+    $('#fr').hide()
+    const myYear = 2001
+    const myMonth = 2
+    var age =new Date().getFullYear()-myYear
+    if(myMonth-(new Date().getMonth()+1)===1){
+        age--
+    }
+    $('.yo').text(age)
+
+    $('#lang').click(()=>{
+        if($('#lang').text()==='fr'){
+            $('#lang').text('en')
+            $('#en').slideToggle(1000)
+            $('#fr').slideToggle(1000)
+        }
+        else if($('#lang').text()==='en'){
+            $('#lang').text('fr')
+            $('#fr').slideToggle(1000)
+            $('#en').slideToggle(1000)
+        }
+    })
+})
